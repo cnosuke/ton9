dd626d7b6ecf07bf2efd0559e8c3b443fb358b29
shimada <shimada@osss.cs.tsukuba.ac.jp>
2012-10-21 01:55:43 +0900

    ・deviseの昨日を実装 ・ヘッダ右上にログイン，ログアウトの項目を追加し，ログイン時はユーザ名を表示するようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Ton9::Application.routes.draw do
-  resources :users do
-    resources :documents do
-      resources :items
+  #resources :users do
+  #  resources :documents do
+  #    resources :items
 
-      scope :binders do
-        post '/'
-        post '/:bid'
-      end
-    end
-  end
+  #    scope :binders do
+  #      post '/'
+  #      post '/:bid'
+  #    end
+  #  end
+  #end
+
+  devise_for :users, :controllers => {
+    :sessions => "users/sessions",
+    :passwords => "users/passwords",
+    :registrations => "users/registrations"
+  }
+
+  root :to => "home#index"
 end
