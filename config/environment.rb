0327e461412d1e21e19898b0a693faf2bd3c3d28
ShinnosukeTAKEDA at CASPER <shinnosuke@gmail.com>
2012-10-20 20:07:24 +0900

    初コミット...ども...

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Ton9::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ton9::Application.config.secret_token = '36262543b7e0537a1ffd9755de9c7260fc26f3d64f2d4610ae908dc21c2307f0e27bef0a8a5d47a6bac143569d3b6d794a2e919502264ec464e74913c6bd8ba9'
