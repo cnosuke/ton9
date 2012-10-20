071e841fa84f4da7a9b6579f1072e98330aa8b5b
Moosan63 <moosan63@Pleiades.local>
2012-10-21 02:02:22 +0900

    なんかゴミをpushしてしまった気がするので修正

--- test/unit/helpers/binders_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BindersHelperTest < ActionView::TestCase
-end
--- test/unit/holder_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HolderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
