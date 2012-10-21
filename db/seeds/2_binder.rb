008e73415bdf52587c495228ca36901392cb5070
Moosan63 <moosan63@Pleiades.local>
2012-10-21 22:31:22 +0900

    seedをbinderの作成に対応。binderはユーザーopentaka以外で共有されます

---
+++ db/seeds/2_binder.rb
@@ -0,0 +1,19 @@
+#coding:utf-8
+binder_sets = ["has-key","shacho","opentaka.binder","mikusan"]
+user_sets = ['moosan','cnosuke','wishid','rkmathi']
+doc_sets = ["アジャイルサムライ","リーダブルコード","ノンデザイナーズ・デザインブック","真夏の夜の淫夢","百年法","シフト表"]
+
+binder_sets.each do |binder|
+  puts "binder sets create"
+  binder = Binder.new(:name => binder)
+  User.all.each do |user|
+    binder.users << user
+  end
+  
+  Document.where(:user_id => User.where(:name => 'moosan' )).each do |document|
+    binder.documents << document
+  end
+  
+  binder.save
+end
+puts binder sets created
