4e6a57f91b2ded5562bf3c0f419597087f0cb72d
Ryuichi KAWAMATA <i@rkmathi.net>
2012-10-21 23:34:33 +0900

    Mod config/unicorn.rb => unicorn.sock

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ working_directory "/var/www/ton9/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/ton9.sock", :backlog => 64
+listen "/tmp/unicorn.sock", :backlog => 64
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
