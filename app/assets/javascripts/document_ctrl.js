12c4715e024f45b39b95924d77f0268609198029
Toshiyuki Shimanuki <nukki@has-key.org>
2012-10-27 19:34:21 +0900

    再帰的に保存する際にビューも更新されるようにした

--- app/assets/javascripts/document_ctrl.js
+++ app/assets/javascripts/document_ctrl.js
@@ -1,8 +1,9 @@
+/*
 $(".open").live('click',function() {
    form = $($(this).parent().find("form")[0]);
    form.toggle();
 });
-
+*/
 function DocumentCtrl($http, $scope, $routeParams) {
   $scope.create = function() {
     $http.post('documents/', { title: this.document.title })
@@ -25,7 +26,6 @@ function DocumentCtrl($http, $scope, $routeParams) {
       .success(function(data) {
         if(data.result == 1) {
           $scope.document = data.data; 
-          console.log($scope.document.items);
         }
         else {
           errorHandling(data.message);
@@ -37,12 +37,19 @@ function DocumentCtrl($http, $scope, $routeParams) {
   };
 
   $scope.additem = function(id) {
+    var elm = this
     $http.post('documents/' + $routeParams.document_id + '/items', {content: this.item.content, parent_item_id: id})
       .success(function(data) {
         if(data.result == 1) {
-          if(!id) $scope.document.items.unshift({item: data.data});
-          $scope.item.body = "";
-          $scope.open = 0;
+          elm.item.content = "";
+          if(!id) {
+            $scope.document.items.unshift({item: data.data});
+          }
+          else {
+            elm.open = 0;
+            addChild($scope.document.items, id, {item: data.data});
+            console.log($scope.document.items);
+          }
         }
         else {
           errorHandling(data.message);
@@ -52,6 +59,23 @@ function DocumentCtrl($http, $scope, $routeParams) {
         errorHandling(data.message);
       });
   };
+
+  function addChild(items, id, data) {
+    if(items == undefined) return;
+    for(var i=0; i<items.length; i++) {
+      item = items[i];
+      if(item.item.id == id) {
+        if(item.children == undefined) {
+          item.children = [data];
+        }
+        else {
+          item.children.push(data);
+        }
+        return;
+      }
+      else addChild(item.children, id, data);
+    }
+  }
 
   $scope.delete = function(i) {
       $http.delete('documents/'+$scope.documents[i].id, { })
@@ -66,8 +90,5 @@ function DocumentCtrl($http, $scope, $routeParams) {
           .error(function() {
               errorHandling(data.message);              
           });
-
-      
   };
-
 }
